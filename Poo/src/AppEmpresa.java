273a20a23af92e409b44a4902f9a1f592e6ef4a7
= <APRENDIZ@BOGCPCDMF020221.FormacionSena.RED>
2024-10-11 07:13:21 -0500

    aun errores

--- Poo/src/AppEmpresa.java
+++ Poo/src/AppEmpresa.java
@@ -1,16 +1,16 @@
 import javax.swing.JOptionPane;
 import Clases.*;
 
-public class App {
+public class AppEmpresa{
     public static void main(String[] args) throws Exception {
-        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de empleados: "))
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cantidad de empleados: "));
         Empresa empresa = new Empresa();
         for(int emp = 0; emp < cantidad; emp++) {
-            String nombre = JOptionPane.showInputDialog(null,"Ingrese el nombre del empleado")
-            String cargo = JOptionPane.showInputDialog(null,"Ingrese el cargo del empleado")
-            double salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el salario del empleado"))
+            String nombre = JOptionPane.showInputDialog(null,"Ingrese el nombre del empleado");
+            String cargo = JOptionPane.showInputDialog(null,"Ingrese el cargo del empleado");
+            double salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el salario del empleado"));
             //adicion de cada empleado a la empresa
-            empresa.contratarEmpleado(new Empleado(nombre,salario,cargo));
+            empresa.contratarEmpleado(new Empleado(nombre,cargo,salario));
         }
 
         System.out.println("El numero total de empleados es: " + empresa.getTotalEmpleados());
